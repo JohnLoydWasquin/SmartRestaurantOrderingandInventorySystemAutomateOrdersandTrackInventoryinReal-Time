1a2824f518a5a7b8d556821b8ed8b920aeeda5f4
John Loyd Ai <johnloydwasquin27@gmail.com>
2024-11-12 22:09:34 +0800

    about

---
+++ about.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/x-icon" href="websiteImage/LogoFP.webp">
+    <title>About Us - Samgyup Paradise</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="headerbar">
+                <div class="account">
+                    <ul>
+                        <a href="main.html">
+                            <li>
+                                <i class="fa-solid fa-house-chimney"></i>
+                            </li>
+                        </a>
+                        <a href="#">
+                             <li>
+                                <i class="fa-solid fa-magnifying-glass searchicon" id="searchicon1"></i>
+                             </li>
+                        </a>
+                        <div class="search" id="searchinput1">
+                            <input type="search" placeholder="Search...">
+                            <i class="fa-solid fa-magnifying-glass srchicon"></i>
+                        </div>
+                        <a href="login.html">
+                            <li>
+                                <i class="fa-solid fa-user" id="user-mb"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <a href="main.html">
+                            <li>Home</li>
+                        </a>
+                        <li>
+                        <a href="#">Menu</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Pork Menu</a></li>
+                            <li><a href="#">Beef Menu</a></li>
+                            <li><a href="#">Chicken Menu</a></li>
+                            <li><a href="#">Side Dishes</a></li>
+                        </ul>
+                        </li>
+                        <a href="about.php">
+                            <li>About</li>
+                        </a>
+                        <a href="bTable.php">
+                            <li>Book Table</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo">
+                <h2>SAMGYUP PARADISE</h2>
+            </div>
+            <div class="bar">
+                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-xmark" id="hdcross"></i>
+            </div>
+            <div class="nav">
+                <ul>
+                    <a href="main.html">
+                        <li>Home</li>
+                    </a>
+                    <li>
+                        <a href="#">Menu</a>
+                        <ul class="dropdown">
+                            <li><a href="menu.php">Pork Menu</a></li>
+                            <li><a href="beefMenu.html">Beef Menu</a></li>
+                            <li><a href="chickenMenu.html">Chicken Menu</a></li>
+                            <li><a href="sideDishes.html">Side Dishes</a></li>
+                        </ul>
+                    </li>
+                    <a href="about.php">
+                        <li>About</li>
+                    </a>
+                    <a href="bTable.php">
+                        <li>Book Table</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="account">
+                <ul>
+                    <a href="main.html">
+                        <li>
+                            <i class="fa-solid fa-house-chimney"></i>
+                        </li>
+                    </a>
+                    <a href="#">
+                        <li>
+                            <i class="fa-solid fa-magnifying-glass searchicon" id="searchicon2"></i>
+                        </li>
+                    </a>
+                    <div class="search" id="searchinput2">
+                        <input type="search" placeholder="Search...">
+                        <i class="fa-solid fa-magnifying-glass srchicon"></i>
+                    </div>
+                    <a href="login.html">
+                        <li>
+                            <i class="fa-solid fa-user" id="user-lap"></i>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    
+</body>
+
+<script src="main.js"></script>
+</html>
